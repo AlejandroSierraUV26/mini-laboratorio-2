9d2ab5fc75f4bfea8d07aa2e032e109ba7e38c65
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-05-03 19:54:31 -0500

    Primer Commit

---
+++ src/logica/Dulce.java
@@ -0,0 +1,46 @@
+package logica;
+public class Dulce {
+    private String nombre;
+    private Categoria categoria;
+    private short cantidad;
+    private short precio;
+    public Dulce(String nombre, Categoria categoria, short cantidad){
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    public short getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(short cantidad) {
+        this.cantidad = cantidad;
+    }
+    public short getPrecio() {
+        return precio;
+    }
+    public void setPrecio(short precio) {
+        this.precio = precio;
+    }
+    public void MostrarDatos(){
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("                         Datos Dulce                          ");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("            Nombre: " + getNombre()+"                            \n" 
+                        +  "            Precio : "+ getPrecio() +"                           \n"
+                        +  "            Categoria : " + getCategoria() +"                \n"                
+                        +  "            Cantidad : "+ getCantidad()+"                       "); 
+        System.out.println("----------------------------------------------------------------");
+    }
+}
